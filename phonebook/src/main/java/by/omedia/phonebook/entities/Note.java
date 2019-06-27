5a85b4eb667dc78836e17f1ec2b339f082e19cde
Сергей <@Sergey>
2019-06-27 21:39:46 +0300

    commit 4

--- phonebook/src/main/java/by/omedia/phonebook/entities/Note.java
+++ phonebook/src/main/java/by/omedia/phonebook/entities/Note.java
@@ -8,47 +8,52 @@ import by.phonebook.proxilayer.INote;
 
 public class Note implements INote{
 
-	private String holderName;
+	private String name;
 	private List<IContact> contacts;
 	
 	public Note() {
 		
 	}
 	
-	public Note(String holderName, List<IContact> contacts) {
-		this.holderName = holderName;
+	public Note(String name, List<IContact> contacts) {
+		this.name = name;
 		this.contacts = contacts;
 	}
 	
 	public String getName() {
-		return this.holderName;
+		return this.name;
 	}
 
 	public List<IContact> getContacts() {
 		return this.contacts;
 	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public void setContacts(List<IContact> contacts) {
+		this.contacts = contacts;
+	}
+	
 	public boolean removeContact(IContact contact) {
 		if(contact == null){
 			return false;
-		} else if(this.contacts.isEmpty()) {
-			return false;
-		}else if(!this.contacts.contains(contact)) {
-			return false;
-		} else {
-			this.contacts.remove(contact);
 		}
-		return true;
+		if(!this.contacts.contains(contact)) {
+			return false;
+		}
+		return this.contacts.remove(contact);
 	}
 
 	public boolean addContact(IContact contact) {
 		if(contact == null) {
 			return false;
-		} else if(this.contacts.contains(contact)){
-			return false;
-		} else {
-			this.contacts.add(contact);
 		}
-		return true;
+		if(this.contacts.contains(contact)){
+			return false;
+		} 
+		return this.contacts.add(contact);
 	}
+	
 }
